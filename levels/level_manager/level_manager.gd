69e1294332c29efe2b90ea6fe3e9e9d2a0f6e75c
Frank Diaz <37251753+frankyd1999@users.noreply.github.com>
2024-07-28 23:31:42 -0700

    Menu Updates

--- levels/level_manager/level_manager.gd
+++ levels/level_manager/level_manager.gd
@@ -10,16 +10,22 @@ func _ready() -> void:
 
 #Avoid Changing states while physics are being processed
 #Calls method during an idle time
-func transistion_next_level():
+func transistion_next_level() -> void:
 	call_deferred("_deferred_transition_next_level")
 
 #Deffered call to tranistion into next level in array
-func _deferred_transition_next_level():
+func _deferred_transition_next_level() -> void:
 	get_child(0).queue_free() #Hopefully Frees Finished Level ¯\_(ツ)_/¯
 	add_child(level_list[level_select].instantiate())
 	level_select += 1
-	
+
 func load_main() -> void:
 	if !main_menu:
 		push_error("Error: No Main Menu")
 	add_child(main_menu.instantiate())
+
+func game_over() -> void:
+	call_deferred("_deffered_game_over")
+
+func _deffered_game_over() -> void:
+	pass
