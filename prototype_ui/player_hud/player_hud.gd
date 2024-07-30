39de6d3dda119ea4d137d0b41c19213811312f6a
Nicholas Sacayan <nksacayan@gmail.com>
2024-07-29 21:34:30 -0700

    Fixes earth armor ui

--- prototype_ui/player_hud/player_hud.gd
+++ prototype_ui/player_hud/player_hud.gd
@@ -8,16 +8,11 @@ extends CanvasLayer
 
 
 func _ready() -> void:
-	var player: PlayerBody = await _get_player()
+	var player: PlayerBody = _get_player()
 	player.earth_armor_activated.connect(_toggle_armor_image)
 
 func _get_player() -> PlayerBody:
-	var current_scene = get_tree().current_scene
-	await current_scene.ready
-	print_debug(current_scene.get_children())
-	if current_scene as LevelManager:
-		current_scene = current_scene.get_child(0)
-	return current_scene.find_child("PlayerBody")
+	return get_parent()
 
 func _toggle_armor_image(armor_enabled: bool) -> void:
 	armor_image.visible = armor_enabled
