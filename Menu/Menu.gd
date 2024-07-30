47dd4c5e8854d7d34352bb41bbb9269a1de9ffa9
Nicholas Sacayan <nksacayan@gmail.com>
2024-07-29 20:13:17 -0700

    themes main menu

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -1,13 +1,14 @@
 extends Control
 
-func _ready():
-	$VBoxContainer/StartButton.grab_focus()
+
+@export var credits_scene: PackedScene
+
 
 func _on_start_button_pressed():
 	get_parent().transistion_next_level()
 
 func _on_credits_button_pressed():
-	get_parent().add_child(load("res://Menu/credits_page.tscn").instantiate())
+	get_parent().add_child(credits_scene.instantiate())
 	self.queue_free()
 
 func _on_quit_button_pressed():
