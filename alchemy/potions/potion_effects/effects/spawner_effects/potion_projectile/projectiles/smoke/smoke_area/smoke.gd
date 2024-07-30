3252bcaf09c979abfe934deec696a2292e7dc9be
Nicholas Sacayan <nksacayan@gmail.com>
2024-07-30 00:16:33 -0700

    updates smoke bomb animation

--- alchemy/potions/potion_effects/effects/spawner_effects/potion_projectile/projectiles/smoke/smoke_area/smoke.gd
+++ alchemy/potions/potion_effects/effects/spawner_effects/potion_projectile/projectiles/smoke/smoke_area/smoke.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
 
-func _on_timer_timeout() -> void:
-	queue_free()
+@onready var _timer: Timer = %Timer
+@onready var _animation: AnimatedSprite2D = %AnimatedSprite2D
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if _animation.animation == "buildup":
+		_animation.play("tension")
+	elif _animation.animation == "disperse":
+		queue_free()
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	if _timer.is_stopped():
+		_animation.play("disperse")
